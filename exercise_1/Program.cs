360f32c8e1a19809412b1a099ed2dd04a7e1a1cd
Yaroslav <104135869+YaroslavGorbachev@users.noreply.github.com>
2022-05-13 08:49:12 +0700

    первое задание

---
+++ exercise_1/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Write(" Введите число (a):  ");
+int a = int.Parse(Console.ReadLine());
+Console.Write(" Введите число (b):  ");
+int b = int.Parse(Console.ReadLine());
+
+int sum = (b * b);
+if (sum == a)
+{
+    Console.WriteLine("Являеться квадратом числа ");
+}
+else
+{
+
+    Console.WriteLine("Неявляеться квадратом числа ");
+}
