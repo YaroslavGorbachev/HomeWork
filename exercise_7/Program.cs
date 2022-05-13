749a5ff078ab545b4254fc21e7956f23707a093a
Yaroslav <104135869+YaroslavGorbachev@users.noreply.github.com>
2022-05-13 09:30:32 +0700

    Седьмое задание

---
+++ exercise_7/Program.cs
@@ -0,0 +1,5 @@
+﻿int Number = new Random().Next(100, 999);
+int a = Number / 100;
+int b = Number % 10;
+Console.WriteLine("Заданное число: " + Number);
+Console.WriteLine("Удалення вторая цифра: " + a + b);
