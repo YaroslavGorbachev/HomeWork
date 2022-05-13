a7e9c7b7658c1e238d464804719dc3b93def90d5
Yaroslav <104135869+YaroslavGorbachev@users.noreply.github.com>
2022-05-13 08:58:04 +0700

    Четвертое задание

---
+++ exercise_4/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите первое число: ");
+int a = int.Parse(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int b = int.Parse(Console.ReadLine());
+if (a % 2 != 0) 
+{
+    a = a+1;
+}     
+for (int n = a; n <= b; n = n + 2)
+{
+     Console.Write(n + " ");
+}
