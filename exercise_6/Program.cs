678278a3b6ca35219076975857103b824c3fd264
Yaroslav <104135869+YaroslavGorbachev@users.noreply.github.com>
2022-05-13 09:07:53 +0700

    шестое залдание

---
+++ exercise_6/Program.cs
@@ -0,0 +1,16 @@
+﻿int number = new Random().Next(10, 99);
+int a = number / 10;
+int b = number % 10;
+Console.WriteLine("Число Отрезка: " + number);
+if (a > b)
+{
+    Console.WriteLine("Наибольшая цифра: " + a);
+}
+else
+{
+    Console.WriteLine("Наибольшая цифра: " + b);
+}
+if (a == b)
+{
+    Console.WriteLine("Цифры равны");
+}
