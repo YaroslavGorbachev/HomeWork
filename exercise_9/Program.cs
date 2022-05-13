fadbbce6784479614b547b6964f8f0d74ae2b047
Yaroslav <104135869+YaroslavGorbachev@users.noreply.github.com>
2022-05-13 09:41:13 +0700

    Девятое задание

---
+++ exercise_9/Program.cs
@@ -0,0 +1,11 @@
+﻿int number = new Random().Next(10, 200);
+if (number / 100 == 0)
+{
+    Console.WriteLine("Заданное число: "+ number);
+    Console.WriteLine("Третьей цифры в числе нет");
+}
+else
+{
+    Console.WriteLine("Заданное число: "+ number);
+    Console.WriteLine("Третья цифра: " + number % 10);
+}
